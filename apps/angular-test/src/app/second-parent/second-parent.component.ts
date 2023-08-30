9d55f80cd23b831749c11e61af97ed43a9600381
Ankit Tanna <ankit.tanna@autogeneral.com.au>
2023-08-30 14:28:17 +1000

    programatic navigating

--- apps/angular-test/src/app/second-parent/second-parent.component.ts
+++ apps/angular-test/src/app/second-parent/second-parent.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'angular-test-second-parent',
@@ -6,7 +7,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./second-parent.component.scss'],
 })
 export class SecondParentComponent implements OnInit {
+  constructor(private router: Router) {}
+
   ngOnInit(): void {
     console.log('SecondParentComponent ngOnInit');
+  }
+
+  navigate(path: string): void {
+    this.router.navigate([path]);
   }
 }
